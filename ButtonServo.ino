c52e2be4667402a2f3468acc60b3eb5129d00e35
rodriguinho <81981576+rjzg@users.noreply.github.com>
2021-05-14 17:52:45 -0300

    .ino file for servo ran by 2 buttons

---
+++ ButtonServo.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+
+Servo s; 
+int pos = 0; 
+int buttonState1 = 0;
+int buttonState2 = 0;
+
+void setup ()
+{
+  s.attach(6);
+  pinMode(10, INPUT);
+  pinMode(8, INPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+
+  buttonState1 = digitalRead(2);
+  buttonState2 = digitalRead(3);
+  
+  s.write(0); 
+  if (buttonState1 == LOW) {
+    pos+=90; 
+    s.write(pos);
+  }
+  
+  if (buttonState2 == LOW) {
+    pos-=90;
+    s.write(pos);
+    
+  }
+
+  delay(500);
+}
